ec869ab10aa65dcfbb3be20eb983a7b5ca22f1d3
Pradosh Shirgaonkar <prashirg@publicisgroupe.net>
2020-05-01 21:47:46 +0530

    Floating button virus animation

--- lib/App.dart
+++ lib/App.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_corona_go/screens/Dashboard.dart';
@@ -16,7 +18,9 @@ class App extends StatefulWidget {
 class _AppState extends State<App> with TickerProviderStateMixin {
   int _selectedIndex = 0;
   AnimationController _controller;
+  AnimationController _controllerChanged;
   String _activeScreen = 'Dashboard';
+  Center _stateVirus;
   final _pageOptions = [Dashboard(), Stats(), ListPage()];
   final _pageNames = ['Dashboard', 'Stats', 'Top Headlines'];
 
@@ -32,12 +36,13 @@ class _AppState extends State<App> with TickerProviderStateMixin {
 
   @override
   void initState() {
+    super.initState();
     _controller = AnimationController(
       duration: const Duration(milliseconds: 5000),
       vsync: this,
     );
-    super.initState();
     _controller.repeat();
+    initialStateWidget();
     checkDataExpiry();
   }
 
@@ -54,7 +59,35 @@ class _AppState extends State<App> with TickerProviderStateMixin {
     });
   }
 
+  void initialStateWidget() {
+    _stateVirus = Center(
+      child: RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+          child: Image.asset(
+            'images/corona_pink.png',
+            height: 200,
+            width: 200,
+          )),
+    );
+  }
+
+  void changedStateWidget() {
+    _stateVirus = Center(
+      child: Image.asset(
+        'images/virus_angry.png',
+        height: 200,
+        width: 200,
+      ),
+    );
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        initialStateWidget();
+      });
+    });
+  }
+
   Widget build(BuildContext context) {
+//    String iconSrc = 'images/corona_pink.png';
     return Scaffold(
         backgroundColor: DarkColor.background,
         appBar: AppBar(
@@ -64,17 +97,14 @@ class _AppState extends State<App> with TickerProviderStateMixin {
           actions: <Widget>[],
         ),
         floatingActionButton: FloatingActionButton(
+          splashColor: Colors.transparent,
           highlightElevation: 0,
-          onPressed: () {},
-          child: Center(
-            child: RotationTransition(
-                turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
-                child: Image.asset(
-                  "images/corona_pink.png",
-                  height: 200,
-                  width: 200,
-                )),
-          ),
+          onPressed: () {
+            setState(() {
+              changedStateWidget();
+            });
+          },
+          child: _stateVirus,
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
