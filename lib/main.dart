8add1708545df4e66eaee97a76c2c34255c8225c
Pradosh Shirgaonkar <prashirg@publicisgroupe.net>
2020-05-05 11:24:31 +0530

    Launcher Icons for android and Ios.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_corona_go/screens/splashVirus.dart';
 import 'package:go_corona_go/themes/theme.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() => runApp(MyApp());
 
